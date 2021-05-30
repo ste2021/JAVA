19344da8ffcbad5e7e67686c79acaffeb86f5a34
Estela Oliveira <estelaoliveira@pecege.com>
2021-05-30 14:30:08 -0300

    Dev: First Commit

---
+++ src/main/java/com/rasmoo/cliente/escola/gradecurricular/GradeCurricular1Application.java
@@ -0,0 +1,13 @@
+package com.rasmoo.cliente.escola.gradecurricular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradeCurricular1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradeCurricular1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/rasmoo/cliente/escola/gradecurricular/GradeCurricular1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rasmoo.cliente.escola.gradecurricular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradeCurricular1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
